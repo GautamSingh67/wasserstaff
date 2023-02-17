ac2a07c8d4ce0b3433615ae28c7bb0480fb52bd1
GautamSingh <lossangle20@gmail.com>
2023-02-17 23:41:44 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 
 import './App.css';
 import FIRSTHEX from './component/FIRSTHEX';
-import { HEXONE } from './component/HEXONE';
-import { HEXSECOND } from './component/HEXSECOND';
+
 
 
 function App() {
   return (
     <div className="App">
        <FIRSTHEX/>
-       <HEXONE/>
-       <HEXSECOND/>
+
     </div>
   );
 }
